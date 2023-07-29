f0f04b3284d27791af0b60b7ac38686fe55c46b1
marikba2k <72571012+marikba2k@users.noreply.github.com>
2023-07-29 15:35:13 +0300

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,24 @@
+const userInput = document.getElementById("userInput");
+
+var expression = "";
+
+function press(num){
+    expression += num;
+    userInput.value = expression;
+}
+
+function equal(){
+    userInput.value = eval(expression);
+    expression = "";
+}
+
+
+
+
+
+function clear1(){
+    console.log("clicked");
+    expression = "";
+    userInput.value = expression;
+}
+
